0fdbb127c33446e015b07892dc88db4910160d72
Jonathan Kelley <jonathankelley87@gmail.com>
2020-05-02 18:47:15 -0400

    updates to markdown js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,47 +4,25 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
-##Descriptions ${data.description}
+##Descriptions ${answer.description}
 
 ##Table of Contents ${answer.tableOfContents}
 
 The README will be populated with the following:
 
-* At least one badge
-* Project title ${answer.title}
-* Description ${answer.description}
-* Table of Contents ${answer.stable}
-* Installation ${answer.installation}
-* Usage ${answer.usage}
-* License ${answer.license}
-* Contributing ${answer.contributing}
-* Tests ${answer.testing}
-* Questions
+### At least one badge
+### Project title : ${answer.title}
+### Description : ${answer.description}
+### Table of Contents ${answer.stable}
+### Installation ${answer.installation}
+### Usage ${answer.usage}
+### License ${answer.license}
+### Contributing ${answer.contributing}
+### Tests ${answer.testing}
+### Questions
   * User GitHub profile picture $data.
   * User GitHub email
-
 `;
-// # ${data["projectTitle"]}
-// ## Description
-// ${data.Description}
-// ## Table of Contects
-// ${data.tableofContects}
-// ## Installation
-// ${data.Installation}
-// ## Usage
-// ${data.Usage}
-// ##  License 
-// ${data.License} 
-// ## Contributing
-// ${data.Contributing}
-// ##  Tests
-// ${data.Tests}
-// ![alt text](${data.image})
-// ## Made by 
-// ${data.username}
-// ## Email
-// ${data.email}
-// `
 };
 
 
